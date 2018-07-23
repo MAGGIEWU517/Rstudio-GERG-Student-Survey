81efcdfdc3f7c9809396ae89c842bf5c1722fc22
MAGGIEWU517 <maggie.wuyuqi@gmail.com>
2018-07-23 16:43:15 -0700

    A priority list of future analysis

---
+++ Further Analysis.R
@@ -0,0 +1,22 @@
+#Further Analysis List
+# (Jared) Fix the G.N tech issue  and make distributions to those compostie variables (Done)
+# (Patrick) created 5 composite variables for personality  (Done)
+# (Joss) corrlation table 
+# (Jared) regression analysis for all variables of those two surveys
+
+
+# (Joss) more discussion about composite distribution 
+# (Joss) Look carefully about Decision_4 
+# 
+# (Patrick)Write a function to report a correlation with one line of text: only two significant digits (no leading zero), 
+#   stars for significance (* p < .05, ** p < .01, *** p < .001) and 95% confidence interval in square brackets (again, two digits, no leading zero).
+# (Patrick)For important correlations, report a scatter plot, with correlation line.
+# (Patrick)When a variable in a correlation has a very skewed distribution (as we saw before) create a new binary variable based on a median split (add .split to the variable name). 
+#   Report the split: what the split point is, how many in each category (you may need to adjust the split point slightly to have a more equal balance between halves). Then report correlation with that binary variable.
+# (Patrick) Ignore unimportant correlation
+
+# (Patrick)If comparing the levels of two measures (e.g. same question in two surveys) report a t-test, again with significance, 95% CI, and also effect size (Cohen's d);
+#   good to write a function to have this on one line of text.
+# (Joss )Mann–Whitney U test
+
+                                                                                                                                                                                                                                           
